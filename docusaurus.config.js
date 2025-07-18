6e9e0af104e85482a5473fe6c3b1dba5adda9fff
蒋普生 <970525549@qq.com>
2025-07-18 10:30:25 +0800

    Configure GitHub Pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,15 +22,17 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://gooselang-dev.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/GooseLang-Doc/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'GooseLang-Dev', // Usually your GitHub org/user name.
+  projectName: 'GooseLang-Doc', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
